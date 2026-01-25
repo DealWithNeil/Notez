29e5be85e780ced8033cfb2334ac19065caebc70
NeilTheRealDeal <neiltherealestdeal@gmail.com>
2026-01-26 00:36:00 +0800

    NoteForm Input Value

--- frontend/src/NoteForm.jsx
+++ frontend/src/NoteForm.jsx
@@ -3,30 +3,29 @@ import { useState } from "react";
 function NoteForm({ onAddNote }) {
   const [text, setText] = useState("");
 
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     if (text.trim() === "") return;
 
     onAddNote(text);
     setText("");
-  }
+  };
 
   return (
-    <form
-      onSubmit={handleSubmit}
-      className="bg-white p-4 rounded shadow mb-4"
-    >
+    <form onSubmit={handleSubmit} className="mb-4">
       <input
         type="text"
-        placeholder="Write a note..."
         value={text}
         onChange={(e) => setText(e.target.value)}
-        className="w-full border p-2 rounded mb-2"
+        placeholder="Write a note..."
+        className="border p-2 mr-2 rounded w-64"
       />
-
-      <button className="bg-blue-500 text-white px-4 py-2 rounded w-full">
-        Add Note
+      <button
+        type="submit"
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Add
       </button>
     </form>
   );
